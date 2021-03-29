a1552cd8318b465bf860761c750cb69ae32b891f
Adan Escamilla <adanescamilla580@gmail.com>
2021-03-29 11:13:44 -0700

    seeds working

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -1,8 +1,11 @@
 class Adventure < ApplicationRecord
-    belongs_to :user
 
-    has_many comments
-    has_many users, through: :comments
+    has_many :comments
+    has_many :users, through: :comments
 
-    # Not sure if this is correct. Can a model both belongs_to and has_many to the same model? The issue is when a User is creating an adventure they need to point back to who created it. Check out seeds.rb
+    # Not sure if this is correct. 
+    #Can a model both belongs_to and has_many to the same model? 
+    #The issue is when a User is creating an adventure they need 
+    #to point back to who created it. Check out seeds.rb
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    has_many comments
-    has_many adventures, through: :comments
+    has_many :comments
+    has_many :adventures, through: :comments
 end

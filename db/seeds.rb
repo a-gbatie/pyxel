39879794042b3c2ecac1b1b02795cd0e376fbbf5
a-gbatie <ange.batie@gmail.com>
2021-04-02 09:25:45 -0700

    backend cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,13 +24,13 @@ u4 = User.create(name: 'William', hometown: 'Denver, Colorado', bio: "Retired sc
 u5 = User.create(name: 'Liam', hometown: 'Seattle, Washington', bio: "Software Engineer lucky enough to live a nomadic, daring life", age: 29)
 
 # a1 user is Alicia
-a1 = Adventure.create(title: 'Hiking', photo: 'https://xdaysiny.com/wp-content/uploads/2017/07/Hiking-Diamond-Head-Honolulu-Oahu-Hawaii.jpg', location: 'Hawaii', hashtags: '#hiking, #hawaii', description: "The scenery is so beautiful, I'm deathly afraid of heights though. Yikes.", likes: 0) 
+a1 = Adventure.create(title: 'Hiking', photo: 'https://xdaysiny.com/wp-content/uploads/2017/07/Hiking-Diamond-Head-Honolulu-Oahu-Hawaii.jpg', location: 'Hawaii', hashtags: '#nature', description: "The scenery is so beautiful, I'm deathly afraid of heights though. Yikes.", likes: 0) 
 
 #a2 user is Liam
-a2 = Adventure.create(title: 'Skydiving', photo: 'https://www.skydive.com.mx/images/gallery/4.jpg', location: 'Mexico', hashtags: '#skydiving, #mexico', description: "Skydiving in Playa Del Carmen was crazy. I even tried a little stunt.", likes: 0) 
+a2 = Adventure.create(title: 'Skydiving', photo: 'https://www.skydive.com.mx/images/gallery/4.jpg', location: 'Mexico', hashtags: '#hardcore', description: "Skydiving in Playa Del Carmen was crazy. I even tried a little stunt.", likes: 0) 
 
 # a3 user is Rachel
-a3 = Adventure.create(title: 'Whitewater Rafting', photo: 'https://i.pinimg.com/originals/c7/34/b1/c734b166cd924a8122915ae5599b7af4.jpg', location: 'Australia', hashtags: '#rafting, #australia', description: "What a rush!", likes: 0) 
+a3 = Adventure.create(title: 'Whitewater Rafting', photo: 'https://i.pinimg.com/originals/c7/34/b1/c734b166cd924a8122915ae5599b7af4.jpg', location: 'Australia', hashtags: '#rafting', description: "What a rush!", likes: 0) 
 
 # 
 c1 = Comment.create(comments: 'So cool! Rafting is definitely in my top 5!', adventure: a3, user: u4)
